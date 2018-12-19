060699a2bb01059e398c3c37ca8f945e3d898e45
Ilya Z <ilyamz@gmail.com>
2018-12-19 08:08:40 -0800

    fix tests

--- pkg/znak/znak_test.go
+++ pkg/znak/znak_test.go
@@ -7,10 +7,10 @@ import (
 )
 
 func testAdd(t *testing.T) {
-	assert = assert.New(t)
+	assert := assert.New(t)
 	assert.Equal(100, add(50, 50), "50 + 50 == 100")
 }
 func testMul(t *testing.T) {
-	assert = assert.New(t)
+	assert := assert.New(t)
 	assert.Equal(99, add(33, 3), "33 * 3 == 99")
 }
